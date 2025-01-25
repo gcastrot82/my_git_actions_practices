8adfafd4b7bb1f41353726a5a77bb6068b553749
Lancelot27 <gcastrot82@hotmail.com>
2025-01-24 22:11:26 -0500

    Update MergeToDevelop

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
-from view.pokedex_view import HealthCkeckView,PokedexGeneralView, PokedexFilteredView
+# from view.pokedex_view import HealthCkeckView,PokedexGeneralView, PokedexFilteredView
 
 app = Flask(__name__)
 
